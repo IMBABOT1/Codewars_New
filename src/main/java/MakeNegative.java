ca017684788dbcfb7d59c64619f5c734b6e356b0
unknown <zlovred12@gmail.com>
2022-08-12 21:53:46 +0300

    ReturnNegative

---
+++ src/main/java/MakeNegative.java
@@ -0,0 +1,11 @@
+public class MakeNegative {
+
+    public static int makeNegative(final int x) {
+        int temp = x;
+        if (temp > 0) {
+            temp *= -1;
+        }
+        return temp;
+    }
+
+}
