9f0a9ebb18f62c18605d4d54bba52b67aceaa59c
unknown <zlovred12@gmail.com>
2022-08-13 21:23:39 +0300

    greet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(SumOfPositive.sum(new int[]{1,2,3,4,5}));
+        TowerBuilder.towerBuilder(3);
     }
 }
