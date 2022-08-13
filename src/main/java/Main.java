28b53fdd3578c34923b895ef1fb7e1da9cfd2795
unknown <zlovred12@gmail.com>
2022-08-13 13:55:00 +0300

    WhoLikesIt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(HighestScoringWord.high("uc, mqnatlrcfil, kmtcybjhgvtg, l"));
+        System.out.println(WhoLikesIt.whoLikesIt(new String[]{}));
     }
 }
