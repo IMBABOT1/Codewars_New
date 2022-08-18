613ce4429dd20f480846943d6a0884fbda64e07b
unknown <zlovred12@gmail.com>
2022-08-18 22:17:26 +0300

    temp

--- src/main/java/SupermarketQueue.java
+++ src/main/java/SupermarketQueue.java
@@ -3,24 +3,23 @@ import java.util.Arrays;
 public class SupermarketQueue {
 
     public static int solveSuperMarketQueue(int[] customers, int n) {
-        int sum = 0;
         int result = 0;
+        int sum = 0;
 
+        System.out.println(Arrays.toString(customers));
+        System.out.println(n);
 
-
-        for (int i = 0; i < customers.length; i++) {
-            sum += customers[i];
-
-            if (customers.length == 1){
-                result = customers[0];
-            }else if (n > sum){
-                result = customers.length;
-            }else {
-                customers[i] = customers[i] - 2;
-                sum +=  customers[i];
+        for (int i = 0; i < customers.length ; i++) {
+            if (customers[i] < n){
+                result  = customers.length;
+            }else if (n == 1){
+                result += customers[i];
+            }else if (customers[i] >= n){
+                result += customers[i];
             }
-
         }
+
+        result = result / n;
 
         return result;
     }
