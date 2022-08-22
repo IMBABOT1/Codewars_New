48c8b9791771fc667042bcb36f60f37a6315f844
unknown <zlovred12@gmail.com>
2022-08-22 16:53:39 +0300

    GrassHopper

---
+++ src/main/java/GetGrade.java
@@ -0,0 +1,19 @@
+public class GetGrade {
+    public static char getGrade(int s1, int s2, int s3) {
+        int avg = (s1 + s2 + s3) / 3;
+
+        if (avg >= 90 && avg <= 100){
+            return 'A';
+        }else if (avg >= 80 && avg < 90){
+            return 'B';
+        }else if (avg >= 70 && avg < 80){
+            return 'C';
+        }else if (avg >= 60 && avg < 70){
+            return 'D';
+        }else if (avg >= 0 && avg < 60){
+            return 'F';
+        }
+
+        throw new RuntimeException("invalid value;");
+    }
+}
