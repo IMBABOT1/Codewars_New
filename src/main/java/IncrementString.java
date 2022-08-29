904b65d638ab51e2e43bc334cbeb2e9e37c958a6
unknown <zlovred12@gmail.com>
2022-08-29 22:26:56 +0300

    test

--- src/main/java/IncrementString.java
+++ src/main/java/IncrementString.java
@@ -4,14 +4,22 @@ public class IncrementString {
         int inc = 0;
         String incValue = "";
 
-        if (last.matches("[0-9]+")){
+
+
+        if (last.matches("[0-8]+")) {
             inc = Integer.parseInt(last);
             inc++;
             incValue = String.valueOf(inc);
-            str = str.substring(0,str.length()-1) + incValue;
+            str = str.substring(0, str.length() - 1) + incValue;
+        } else if (last.equals("9")) {
+            String str1 = str.replaceAll("\\D+", "");
+            int inc9 = Integer.parseInt(str1);
+            inc9++;
+            str = str.replaceAll("\\d", "");
+            str = str + inc9;
+        } else {
+            str = str.substring(0, str.length()) + "1";
         }
-
-
-        return str; // you code here
+        return str;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(IncrementString.incrementString("foobar000"));
+        System.out.println(IncrementString.incrementString("foobar99"));
     }
  }
