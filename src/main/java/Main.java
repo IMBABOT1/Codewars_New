564ef14f580d55107b269fe7326eaf220c7d776c
unknown <zlovred12@gmail.com>
2022-08-29 20:14:00 +0300

    OppositesAttract

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(SquareSum.squareSum(new int[] {1,2,2}));
+        System.out.println(OppositesAttract.isLove(8, 9));
     }
  }
