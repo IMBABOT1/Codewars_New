a4d59265f0180432b7515834863c72ab2a38f38c
unknown <zlovred12@gmail.com>
2022-08-30 22:08:10 +0300

    lesson78

--- src/main/java/IncrementString.java
+++ src/main/java/IncrementString.java
@@ -1,25 +1,27 @@
 public class IncrementString {
-    public static String incrementString(String str) {
-        String last = str.substring(str.length() - 1);
-        int inc = 0;
-        String incValue = "";
-
-
-
-        if (last.matches("[0-8]+")) {
-            inc = Integer.parseInt(last);
-            inc++;
-            incValue = String.valueOf(inc);
-            str = str.substring(0, str.length() - 1) + incValue;
-        } else if (last.equals("9")) {
-            String str1 = str.replaceAll("\\D+", "");
-            int inc9 = Integer.parseInt(str1);
-            inc9++;
-            str = str.replaceAll("\\d", "");
-            str = str + inc9;
-        } else {
-            str = str.substring(0, str.length()) + "1";
-        }
-        return str;
-    }
+//    public static String incrementString(String str) {
+//        if (str.equals("")){
+//            return "1";
+//        }else {
+//
+//        String last = str.substring(str.length() - 1);
+//        int inc = 0;
+//        String incValue = "";
+//
+//        if (last.matches("[0-8]+")) {
+//            inc = Integer.parseInt(last);
+//            inc++;
+//            incValue = String.valueOf(inc);
+//            str = str.substring(0, str.length() - 1) + incValue;
+//        } else if (last.equals("9")) {
+//            String str1 = str.replaceAll("\\D+", "");
+//            int inc9 = Integer.parseInt(str1);
+//            inc9++;
+//            str = str.replaceAll("\\d", "");
+//            str = str + inc9;
+//        } else {
+//            str = str.substring(0, str.length()) + "1";
+//        }
+//        return str;
+//    }
 }
---
+++ src/main/java/RoundToTheNextMultipleOf5.java
@@ -0,0 +1,7 @@
+public class RoundToTheNextMultipleOf5 {
+
+    public static int roundToNext5(int number) {
+         return (number + 4) / 5 * 5
+    }
+}
+
