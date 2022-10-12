1ea85fd3744846155439c0221a3507f9057db046
unknown <zlovred12@gmail.com>
2022-10-12 17:04:48 +0300

    getVolumeOfCuboid

---
+++ src/main/java/GetVolumeOfCuboid.java
@@ -0,0 +1,6 @@
+public class GetVolumeOfCuboid {
+
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return (width + height) * length;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(RepeatStr.repeatStr(4, "a"));
+        System.out.println(GetVolumeOfCuboid.getVolumeOfCuboid(6.3, 2, 5));
     }
  }
