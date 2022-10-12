a1eb1abd1e8900febbebdab0afe572cd32a00dd6
unknown <zlovred12@gmail.com>
2022-10-12 17:51:23 +0300

    factorial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(GetVolumeOfCuboid.getVolumeOfCuboid(6.3, 2, 5));
+        System.out.println(Factorial.factorial(5));
     }
  }
