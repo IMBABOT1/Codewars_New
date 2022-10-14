fbdb70df6839130b9c2302bcdae96247e38ccc05
unknown <zlovred12@gmail.com>
2022-10-14 13:28:44 +0300

    StrCount

--- src/main/java/StringToArray.java
+++ src/main/java/StringToArray.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class StringToArray {
 
     public static String[] stringToArray(String s) {
-        String[] arr = s.split(" ");
-        return arr;
+        return s.split(" ");
     }
 }
