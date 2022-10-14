8fd759e8f1bb385269afa454497077d5dc67b2a8
unknown <zlovred12@gmail.com>
2022-10-14 22:21:14 +0300

    WideMouthedFrog

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ArrayPrinter.printArray(new Integer[]{1,2,3,4}));
+        System.out.println(WideMouthedFrog.mouthSize("alligator"));
     }
 }
