ffd825bab488ee8be3abf9da9c988e37ff761f92
unknown <zlovred12@gmail.com>
2022-10-15 16:09:36 +0300

    temp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ProdFib.productFib(2932589879121l));
+        System.out.println(ProdFib.productFib(55500));
     }
 }
--- src/main/java/ProdFib.java
+++ src/main/java/ProdFib.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -6,20 +7,14 @@ public class ProdFib {
     public static long[] productFib(long prod) {
         long[] result = new long[3];
 
-        System.out.println(prod);
-
         int a;
         int b = 0;
         int c = 1;
 
         int first = 0;
         int second = 0;
-        int sum = 0;
 
-        int firstIndex = 0;
-        int secondIndex = 0;
 
-        StringBuilder sb = new StringBuilder();
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < prod; i++) {
             a = b;
@@ -28,32 +23,28 @@ public class ProdFib {
             list.add(c);
         }
 
+
         int index = 0;
 
-        while (sum <= prod) {
-            if (index == list.size() - 1) {
-                break;
-            }
+        while (true) {
             first = list.get(index);
             second = list.get(index + 1);
             if (first * second == prod) {
-                firstIndex = index - 1;
-                secondIndex = index;
-                result[0] = list.get(firstIndex + 1);
-                result[1] = list.get(secondIndex + 1);
+                result[0] = list.get(index);
+                result[1] = list.get(index + 1);
                 result[2] = 1;
                 break;
             } else if (first * second > prod) {
-                firstIndex = index - 1;
-                secondIndex = index;
-                result[0] = list.get(firstIndex + 1);
-                result[1] = list.get(secondIndex + 1);
+                result[0] = list.get(index);
+                result[1] = list.get(index + 1);
                 result[2] = 0;
                 break;
             }
             index++;
         }
 
+
         return result;
+
     }
 }
