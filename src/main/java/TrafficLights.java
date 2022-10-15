df983ceea66ad213eff91025b4d1dcb3e70c3417
unknown <zlovred12@gmail.com>
2022-10-15 22:10:29 +0300

    TrafficLights

---
+++ src/main/java/TrafficLights.java
@@ -0,0 +1,14 @@
+public class TrafficLights {
+
+    public static String updateLight(String current) {
+        switch (current){
+            case ("green"):
+                return "yellow";
+            case ("red"):
+                return "green";
+            case ("yellow"):
+                return "red";
+        }
+        return "";
+    }
+}
